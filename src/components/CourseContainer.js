cb9a7ec170d4432929a91dffa922f168917ac4ab
laschuet <laschuet@users.noreply.github.com>
2017-04-01 18:26:21 +0200

    Reduce code

--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -2,29 +2,26 @@ import React from 'react';
 
 import Course from 'components/Course';
 
-const CourseContainer = props => {
-  const courseNodes = props.data.map(course =>
-    <Course
-      key={course.key}
-      title={course.title}
-      subtitle={course.subtitle}
-      level={course.level}
-    />
-  );
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Subtitle</th>
-          <th>Level</th>
-        </tr>
-      </thead>
-      <tbody>
-        {courseNodes}
-      </tbody>
-    </table>
-  );
-};
+const CourseContainer = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Subtitle</th>
+        <th>Level</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.data.map(course => (
+        <Course
+          key={course.key}
+          title={course.title}
+          subtitle={course.subtitle}
+          level={course.level}
+        />
+      ))}
+    </tbody>
+  </table>
+);
 
 export default CourseContainer;
