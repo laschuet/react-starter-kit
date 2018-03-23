53e72940cd4b5e21379c47fb60239f2a27f26b74
laschuet <laschuet@users.noreply.github.com>
2018-03-23 20:40:15 +0100

    Remove extra level

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
 import createHistory from 'history/createBrowserHistory';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
-import { routerMiddleware } from 'react-router-redux';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
-import Root from './components/Root';
+import Application from './components/Application';
 import reducer from './reducers/index';
 
 /* eslint-disable no-underscore-dangle */
@@ -23,7 +24,11 @@ const domElement = document.getElementById('react');
 const render = () => {
   ReactDOM.render(
     <AppContainer>
-      <Root history={history} store={store} />
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Application />
+        </ConnectedRouter>
+      </Provider>
     </AppContainer>,
     domElement,
   );
@@ -32,7 +37,7 @@ const render = () => {
 render();
 
 if (module.hot) {
-  module.hot.accept('./components/Root', () => {
+  module.hot.accept('./components/Application', () => {
     render();
   });
 }
