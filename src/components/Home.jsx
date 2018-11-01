117a504083f7535989e287a40bcb507b667b4897
laschuet <laschuet@users.noreply.github.com>
2018-11-01 08:27:03 +0100

    Use shorthand notation for fragments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import styles from '../stylesheets/components/common/typography.css';
 
 const Home = () => (
-  <React.Fragment>
+  <>
     <h1 className={styles.heading}>Home</h1>
-  </React.Fragment>
+  </>
 );
 
 export default Home;
