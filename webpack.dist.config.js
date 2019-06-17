bc0ac7bdff5feed7c27a27a1b877d17fd3bc1b95
laschuet <laschuet@users.noreply.github.com>
2019-06-17 21:15:44 +0200

    Remove redundant setting

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -71,9 +71,6 @@ const config = {
     },
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-    }),
     new MiniCssExtractPlugin({
       filename: filenames.dist.css,
     }),
