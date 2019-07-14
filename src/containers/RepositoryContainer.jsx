9b1ba6d622b4f7be1df3dd63a052c430cee046a3
laschuet <laschuet@users.noreply.github.com>
2019-07-14 23:31:00 +0200

    Transform class component

--- src/containers/RepositoryContainer.jsx
+++ src/containers/RepositoryContainer.jsx
@@ -1,55 +1,39 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import fetchRepositories from '../actions/repositories';
 import Repository from '../components/Repository';
 
-class RepositoryContainer extends React.Component {
-  static propTypes = {
-    fetchRepositories: PropTypes.func.isRequired,
-    isFetching: PropTypes.bool.isRequired,
-    repositories: PropTypes.arrayOf(PropTypes.shape(Repository.propTypes))
-      .isRequired,
-  };
+const RepositoryContainer = () => {
+  const dispatch = useDispatch();
+  const isFetching = useSelector(state => state.repositories.isFetching);
+  const repositories = useSelector(state => state.repositories.entities);
 
-  componentDidMount() {
-    this.props.fetchRepositories('https://api.github.com/users/laschuet/repos');
-  }
+  useEffect(() => {
+    dispatch(fetchRepositories('https://api.github.com/users/laschuet/repos'));
+  }, []);
 
-  render() {
-    return this.props.isFetching ? (
-      <div>Loading...</div>
-    ) : (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.repositories.map(repo => (
-            <Repository
-              key={repo.id}
-              name={repo.name}
-              description={repo.description}
-            />
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-}
+  return isFetching ? (
+    <div>Loading...</div>
+  ) : (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {repositories.map(repo => (
+          <Repository
+            key={repo.id}
+            name={repo.name}
+            description={repo.description}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-const mapStateToProps = state => ({
-  isFetching: state.repositories.isFetching,
-  repositories: state.repositories.entities,
-});
-
-export default connect(
-  mapStateToProps,
-  {
-    fetchRepositories,
-  },
-)(RepositoryContainer);
+export default RepositoryContainer;
